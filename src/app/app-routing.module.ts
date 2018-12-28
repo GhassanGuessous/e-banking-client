ad7ae70f16ae409da6aa0e099c18c918082066b7
test <ayoubbahmad@gmail.com>
2018-12-28 00:03:32 -0500

    create login page and configure routes + navbar test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { from } from 'rxjs';
+const routes: Routes = [
+  {path:'navbar', component : NavbarComponent},
+  {path:'login', component : LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
