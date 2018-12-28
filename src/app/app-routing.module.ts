428d85b39521dfa2ad42461c51d7dd1ecbe74c99
test <ayoubbahmad@gmail.com>
2018-12-28 00:47:04 -0500

    create register page and configure its route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 import { from } from 'rxjs';
 const routes: Routes = [
   {path:'navbar', component : NavbarComponent},
-  {path:'login', component : LoginComponent}
+  {path:'login', component : LoginComponent},
+  {path:'register', component : SignupComponent},
 ];
 
 @NgModule({
